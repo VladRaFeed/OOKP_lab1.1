04b9f26978b11a82b9ea1244359588ab36867f30
VladRaFeed <122728801+VladRaFeed@users.noreply.github.com>
2025-02-12 16:05:04 +0200

    work work

---
+++ src/main.js
@@ -0,0 +1,195 @@
+import $ from "jquery";
+import Notiflix from 'notiflix';
+
+const formWrapper = document.querySelector('.form-wrapper');
+
+class physikTask {
+    constructor(v, t) {
+        this.speed = Number(v);
+        this.time = Number(t);
+    }
+
+    solvePhysicTask() {
+        console.log('The path is ' + this.speed + ' and the time is ' + this.time);
+    }
+}
+
+class waySearchTask extends physikTask {
+    constructor(v, t) {
+        super(v, t);
+    }
+
+    solveWaySearchTask() {
+        // if(typeof this.speed === "number" && typeof this.time === "number" && this.speed > 0 && this.time > 0) {
+            console.log("YES")
+            const ress = this.speed / this.time;
+            return ress;
+
+        // } else {
+            // Notiflix.Notify.success('Please enter a number');
+        // }
+    }
+}
+
+class waySearchTask2 extends physikTask {
+    constructor(v, t, a) {
+        super(v, t);
+        this.acceleration = Number(a);
+    }
+
+    solveWaySearchTask() {
+        const ress = this.speed * this.time + (0.5 * this.acceleration * (this.time * this.time));
+        return ress;
+    }
+}
+
+class densitySearch extends physikTask {
+    constructor(m, v) {
+        super(m, v);
+        this.mass = Number(m);
+        this.volume = Number(v);
+    }
+
+    solveDensitySearch() {
+        const ress = this.mass / this.volume;
+        return ress;
+    }
+}
+
+const task1Solve = () => {
+    const form = document.querySelector('.taskForm');
+    const resultText = document.querySelector('.taskResult');
+    const speed = document.getElementById('speed').value;
+    const time = document.getElementById('time').value;
+    
+    const task1 = new waySearchTask(speed, time);
+    const ress = task1.solveWaySearchTask();
+    resultText.innerHTML = ``;
+    resultText.innerHTML = `Result is ${ress}`;
+}
+
+const task1Markup = () => {
+    formWrapper.innerHTML = `
+    <p class="taskLabel">Розрахунок рівномірного прямолінійного руху</p>
+        <form class="taskForm">
+            <div class="input-wrapper">
+                <label for="speed" class="form-label">Enter speed:</label>
+                <input type="number" class="form-input" id="speed" required>
+            </div>
+            <div class="input-wrapper">
+                <label for="time" class="form-label">Enter time:</label>
+                <input type="number" class="form-input" id="time" required>
+            </div>
+            <button type="button" class="taskBtn task1Btn">Calculate</button>
+            <p class="taskResult"></p>
+        </form>
+    `;
+    const acceptBtn = document.querySelector('.task1Btn');
+    acceptBtn.addEventListener('click', task1Solve);
+}
+
+const task2Solve = () => {
+    const form = document.querySelector('.taskForm');
+    const resultText = document.querySelector('.taskResult');
+    const speed = document.getElementById('speed').value;
+    const time = document.getElementById('time').value;
+    const acceleration = document.getElementById('acceleration').value;
+    
+    const task2 = new waySearchTask2(speed, time, acceleration);
+    const ress = task2.solveWaySearchTask();
+    resultText.innerHTML = ``;
+    resultText.innerHTML = `Result is ${ress}`;
+}
+
+const task2Markup = () => {
+    formWrapper.innerHTML = `
+    <p class="taskLabel">Розрахунок рівнозмінного прямолінійного руху</p>
+        <form class="taskForm">
+            <div class="input-wrapper">
+                <label for="speed" class="form-label">Enter speed:</label>
+                <input type="number" class="form-input" id="speed" required>
+            </div>
+            <div class="input-wrapper">
+                <label for="time" class="form-label">Enter time:</label>
+                <input type="number" class="form-input" id="time" required>
+            </div>
+            <div class="input-wrapper">
+                <label for="acceleration" class="form-label">Enter acceleration:</label>
+                <input type="number" class="form-input" id="acceleration" required>
+            </div>
+            <button class="taskBtn task2Btn" type="button">Calculate</button>
+            <p class="taskResult"></p>
+        </form>
+    `;
+    
+
+    const acceptBtn = document.querySelector('.task2Btn');
+    acceptBtn.addEventListener('click', task2Solve);
+}
+
+const task3Solve = () => {
+    const form = document.querySelector('.taskForm');
+    const resultText = document.querySelector('.taskResult');
+    const mass = document.getElementById('mass').value;
+    const volume = document.getElementById('volume').value;
+    
+    const task3 = new densitySearch(mass, volume);
+    const ress = task3.solveDensitySearch();
+    resultText.innerHTML = ``;
+    resultText.innerHTML = `Result is ${ress}`;
+}
+
+const task3Markup = () => {
+    formWrapper.innerHTML = `
+    <p class="taskLabel">Розрахунок щільності</p>
+        <form class="taskForm">
+            <div class="input-wrapper">
+                <label for="mass" class="form-label">Enter mass:</label>
+                <input type="number" class="form-input" id="mass" required>
+            </div>
+            <div class="input-wrapper">
+                <label for="volume" class="form-label">Enter volume:</label>
+                <input type="number" class="form-input" id="volume" required>
+            </div>
+            <button class="taskBtn task3Btn" type="button">Calculate</button>
+            <p class="taskResult"></p>
+        </form>
+    `;
+
+    const acceptBtn = document.querySelector('.task3Btn');
+    acceptBtn.addEventListener('click', task3Solve);
+}
+
+/*Dropdown Menu*/
+$('.dropdown').click(function () {
+    $(this).attr('tabindex', 1).focus();
+    $(this).toggleClass('active');
+    $(this).find('.dropdown-menu').slideToggle(300);
+});
+$('.dropdown').focusout(function () {
+    $(this).removeClass('active');
+    $(this).find('.dropdown-menu').slideUp(300);
+});
+$('.dropdown .dropdown-menu li').click(function () {
+    const selectedText = $(this).text();
+    const selectedId = $(this).attr('id');
+    $(this).parents('.dropdown').find('span').text(selectedText);
+    $(this).parents('.dropdown').find('input').attr('value', selectedId);
+    const result = selectedId;
+    switch (result) {
+        case "Task1":
+            formWrapper.innerHTML= '';
+            task1Markup();
+            break;
+        case "Task2":
+            formWrapper.innerHTML= '';
+            task2Markup();
+            break;
+        case "Task3":
+            formWrapper.innerHTML= '';
+            task3Markup();
+            break;
+        default:
+            console.log("No task selected");
+    }
+});
